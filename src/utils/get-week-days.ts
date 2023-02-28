fefa47fe0d6f9630bc175bab04c822f26892ca69
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-02-28 10:02:46 -0300

    feat: Função para dias do calendário

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,4 +1,8 @@
-export function getWeekDays() {
+interface IGetWeekDaysParams {
+   short?: boolean;
+}
+
+export function getWeekDays({ short = false }: IGetWeekDaysParams = {}) {
    const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' });
 
    return Array
@@ -6,6 +10,10 @@ export function getWeekDays() {
       .map(day => {
          const weekDay = formatter.format(new Date(Date.UTC(2021, 5, day)));
 
+         if (short) {
+            return weekDay.substring(0, 3).toUpperCase();
+
+         }
          return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1));
       });
 }
