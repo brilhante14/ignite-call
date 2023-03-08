64979a6170f7eaf05b9f74acb733946599f148f8
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-08 19:12:37 -0300

    feat: Configurando default seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,7 @@ import { queryClient } from '@/lib/react-query';
 import { globalStyles } from '@/styles/global';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 
 import "../lib/dayjs";
@@ -12,6 +13,14 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.com.br/',
+            siteName: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
